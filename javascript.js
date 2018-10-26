64430706dbc61e43c767307e5be8fab0b9bdf38c
smwhite <smwhite@nevada.unr.edu>
2018-10-26 13:51:13 -0700

    Style Changes
    Changed the sections style up a bit. Biggest change is that the height has been changed to take the full screen per section.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+// smooth scroll
 $(document).ready(function(){
 	// Add smooth scrolling to all links
 	$("a").on('click', function(event){
@@ -9,7 +11,7 @@ $(document).ready(function(){
 			var hash = this.hash;
 			
 			// Using jQuery's animate() method to add smooth page scroll
-			$('html, body,wrapper,sidbar').animate({scrollTop: $(hash).offset().top}, 800, function(){
+			$('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
 				// Add hash (#) to URL 
 				window.location.hash = hash;
 			});
